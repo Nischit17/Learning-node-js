1f48100c61cb9ebbc8872c43c522d02c12b193c8
Nischit17 <nischit819@gmail.com>
2025-05-30 18:42:42 +0530

    basic CRUD operations of a book-store

---
+++ 11.Bookstore-API/controllers/book-controller.js
@@ -0,0 +1,137 @@
+const Book = require("../models/book");
+
+const getAllBooks = async (req, res) => {
+  try {
+    const allBooks = await Book.find({});
+    if (allBooks.length > 0) {
+      res.status(200).json({
+        success: true,
+        message: "List of books fetched successfully!",
+        data: allBooks,
+      });
+    } else {
+      res.status(404).json({
+        success: false,
+        message: "Books not found, please add books to see list of all books!",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Something went wrong please try again.",
+    });
+  }
+};
+
+const getSingleBookById = async (req, res) => {
+  try {
+    const getCurrentBookID = req.params.id;
+
+    const bookDetailsByID = await Book.findById(getCurrentBookID);
+    if (!bookDetailsByID) {
+      return res.status(404).json({
+        success: false,
+        message: "Book with the current ID is not found!",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Got single book by ID",
+      data: bookDetailsByID,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Something went wrong please try again.",
+    });
+  }
+};
+
+const addNewBook = async (req, res) => {
+  try {
+    const newBookFormData = req.body;
+
+    const newlyCreatedBook = await Book.create(newBookFormData);
+
+    if (newlyCreatedBook) {
+      res.status(201).json({
+        success: true,
+        message: "Book added successfully",
+        data: newlyCreatedBook,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Something went wrong please try again.",
+    });
+  }
+};
+
+const updateBook = async (req, res) => {
+  try {
+    const updatedBookFormData = req.body;
+    const getCurrentBookID = req.params.id;
+    const updatedBook = await Book.findByIdAndUpdate(
+      getCurrentBookID,
+      updatedBookFormData,
+      {
+        new: true,
+      }
+    );
+
+    if (!updateBook) {
+      res.status(404).json({
+        success: false,
+        message: "Book is not found with this ID",
+      });
+    }
+    res.status(200).json({
+      success: true,
+      message: "Book updated successfully",
+      data: updatedBook,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Something went wrong please try again.",
+    });
+  }
+};
+
+const deleteBook = async (req, res) => {
+  try {
+    const getCurrentBookID = req.params.id;
+    const deletedBook = await Book.findByIdAndDelete(getCurrentBookID);
+    if (!deleteBook) {
+      res.status(404).json({
+        success: false,
+        message: "Book is not found with this ID",
+      });
+    }
+    res.status(200).json({
+      success: true,
+      message: "Book deleted successfully",
+      data: deletedBook,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Something went wrong please try again.",
+    });
+  }
+};
+
+module.exports = {
+  getAllBooks,
+  getSingleBookById,
+  addNewBook,
+  updateBook,
+  deleteBook,
+};
--- 11.Bookstore-API/server.js
+++ 11.Bookstore-API/server.js
@@ -1,16 +1,20 @@
 require("dotenv").config();
 const express = require("express");
 const connectToDB = require("./database/db");
+const bookRoutes = require("./routes/book-routes");
 
 const app = express();
 
-const PORT = process.env.PORT || 3010;
+const PORT = process.env.PORT || 8080;
 
 // Connect to our database
 connectToDB();
 
 // Middleware -> express.json()
 app.use(express.json());
+
+// Routes
+app.use("/api/books", bookRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is now running on PORT : ${PORT}`);
